a489ad4374902da0a87e9090d1111ee4ade74656
Guilherme Silva <guilherk.dsilva@gmail.com>
2022-09-14 14:38:49 -0300

    project finished

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ let topRight = document.getElementById("topright")
 let botLeft = document.getElementById("botleft")
 let botRight = document.getElementById("botright")
 
+let copyResult = document.getElementById("borderCopy")
+
+let copyBtn = document.getElementById("buttonCopy")
+
 topRight.addEventListener("input", function(evt){ 
     let trValue = evt.target.value
     box.style.borderTopRightRadius = `${trValue}px`
@@ -22,6 +26,12 @@ botRight.addEventListener("input", function(evt){
     box.style.borderBottomRightRadius = `${trValue}px`
 })
 
-let boxCS = window.getComputedStyle(box)
+function copyToClipboard(){
+    navigator.clipboard.writeText(copyResult.value).then(() => {
+        alert("copiado!")
+    })
+}
 
-console.log(boxCS.borderRadius);
+setInterval(() => {
+    copyResult.value = String(`border-radius:${box.style.borderRadius}`)
+}, 500);
